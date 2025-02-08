99c3f0615616d8ea730616a88de1494311889866
Lars Schumacher <schumacher90lars@gmail.com>
2025-02-08 23:56:10 +0100

    added new components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { MySalesComponent } from './my-sales/my-sales.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'foryou', component: MainContentComponent },
+  { path: "favorite", component: FavoriteComponent },
+  { path: "mysales", component: MySalesComponent }
+];
