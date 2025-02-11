f1c5a0bbc07beeb4032304fc1558112f63277b20
Lars Schumacher <schumacher90lars@gmail.com>
2025-02-11 10:13:42 +0100

    favorite content

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './favorite.component.html',
   styleUrl: './favorite.component.scss'
 })
 export class FavoriteComponent {
+  images = [
+    '/secondCCM.png',
+    '/secondCCM.png',
+    '/secondCCM.png',
+    '/secondCCM.png',
+    '/secondCCM.png',
+  ];
+  currentIndex = 0;
 
 }
